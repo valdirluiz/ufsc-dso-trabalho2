272860938605b8f051808a78da2ad227b9c41028
Valdir Luiz <arnholdvaldir@gmail.com>
2016-05-26 20:14:16 -0300

    add fonte

---
+++ ufsc-dso-trabalho2-ejb/build/generated-sources/ap-source-output/br/ufsc/ine/dso/models/Cliente_.java
@@ -0,0 +1,19 @@
+package br.ufsc.ine.dso.models;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-26T20:07:40")
+@StaticMetamodel(Cliente.class)
+public class Cliente_ { 
+
+    public static volatile SingularAttribute<Cliente, String> senha;
+    public static volatile SingularAttribute<Cliente, Date> datacadastro;
+    public static volatile SingularAttribute<Cliente, Integer> quantidadepontos;
+    public static volatile SingularAttribute<Cliente, String> nome;
+    public static volatile SingularAttribute<Cliente, Integer> id;
+    public static volatile SingularAttribute<Cliente, String> login;
+
+}
